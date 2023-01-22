26d5d9806d70fbd4c6c7c8e4b30081294a86ff09
ASKqqq <akalinin33@gmail.com>
2023-01-22 16:56:02 +0500

    add stayles header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,28 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Link } from 'react-router-dom'
+import logo from '../../img/logo.png'
+import headerStayles from './Header.module.css'
 
 export const Header = () => (
   <header>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">HOME</Link>
-        </li>
-        <li>
-          <Link to="/signin">Войти</Link>
-        </li>
-        <li>
-          <Link to="/signup">Регистрация</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="container">
+      <div className={headerStayles.wrapper}>
+        <div className={headerStayles.wraplogo}>
+          <Link to="/">
+            <img src={logo} className="logo" alt="logo" />
+          </Link>
+        </div>
+        <nav className={headerStayles.nav}>
+          <ul>
+            <li>
+              <Link to="/signin">Войти</Link>
+            </li>
+            <li>
+              <Link to="/signup">Регистрация</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   </header>
 )
