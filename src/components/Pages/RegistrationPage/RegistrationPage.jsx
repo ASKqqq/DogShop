6ae662bdd4bd94dadd0a480a68f54dabf39d61db
ASKqqq <akalinin33@gmail.com>
2023-01-25 22:37:17 +0500

    changed RegPage

--- src/components/Pages/RegistrationPage/RegistrationPage.jsx
+++ src/components/Pages/RegistrationPage/RegistrationPage.jsx
@@ -55,31 +55,3 @@ export const RegistrationPage = () => (
     </Form>
   </Formik>
 )
-// <div className="container">
-//   <form>
-//     <div className={regStyles.group}>
-//       <input type="email" placeholder="name@example.com" />
-//       <span className={regStyles.highlight} />
-//       <span className="bar" />
-//     </div>
-//     <div className={regStyles.group}>
-//       <input type="text" placeholder="sm9" />
-//       <span className={regStyles.highlight} />
-//       <span className="bar" />
-//     </div>
-//     <div className={regStyles.group}>
-//       <input type="password" placeholder="password here" />
-//       <span className={regStyles.highlight} />
-//       <span className="bar" />
-//     </div>
-//     <button
-//       type="button"
-//       className={classNames(regStyles.button, regStyles.buttonBlue)}
-//     >
-//       Sign Up
-//       <div className={classNames(regStyles.ripples, regStyles.buttonRipples)}>
-//         <span className={regStyles.ripplesCircle} />
-//       </div>
-//     </button>
-//   </form>
-// </div>
