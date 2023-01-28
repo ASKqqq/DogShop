a2ecd8a9cd3145c26ff4ae3ba370a51a3b2c8c20
ASKqqq <akalinin33@gmail.com>
2023-01-29 01:39:57 +0500

    task2 done

--- src/components/Pages/RegistrationPage/RegistrationPage.jsx
+++ src/components/Pages/RegistrationPage/RegistrationPage.jsx
@@ -57,7 +57,7 @@ export const RegistrationPage = () => {
             type="submit"
             className={classNames(regStyles.button, regStyles.buttonBlue)}
           >
-            Sign
+            Sign Up
             <div
               className={classNames(regStyles.ripples, regStyles.buttonRipples)}
             >
